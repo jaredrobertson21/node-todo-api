eb80a8e5ba09108f13d17ce553ee48ac5f2d0051
Jared Robertson <jared.robertson@usask.ca>
2017-12-27 21:50:52 -0600

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -30,6 +31,30 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 })
+
+// GET /todos/1234756
+// :id is a 'url parameter'
+app.get('/todos/:id', (req, res) => {
+    // access the :id passed in with req.params.id
+    var id = req.params.id;
+
+    // validate id
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    // findById
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            return res.status(200).send({todo});
+        }
+        
+        return res.status(404).send();
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
 
 app.listen(tempPort, () => {
     console.log(`Started on ${tempPort}`)
