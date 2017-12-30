0946d4e7dd27acfc0f0bd63835834bc85db69ce2
Jared Robertson <jared.robertson@usask.ca>
2017-12-30 11:58:22 -0600

    Add DELETE todos route and respective tests

--- server/server.js
+++ server/server.js
@@ -56,6 +56,27 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id
+
+    // validate the id
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    // remove todo by id
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        return res.status(200).send({todo});
+    }, (e) => {
+        console.log(`There was an error: ${e}`);
+        return res.status(400).send()
+    })
+})
+
 app.listen(port, () => {
     console.log(`Started on ${port}`)
 });
